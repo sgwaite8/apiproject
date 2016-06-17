4bf39f9f7a85917a8e45577e97695f0fb72b4a98
Michael Malleske <mmalleske@gmail.com>
2016-06-17 13:37:09 -0500

    MVP more dry

--- api.js
+++ api.js
@@ -1,27 +1,54 @@
 var prompt = require('prompt');
 var open = require('open');
-
-// 1. Welcome the user to our application
-console.log("Welcome to the Movie Info Finder!");
-// 2. Prompt the user to select a movie
-prompt.start();
-prompt.get(['movieName'], function (err, result) {
-  console.log('You picked ' + result.movieName + '!');
-// 3. Make a GET request to the OMDb API for the movie information
-  var request = require('request');
-  request('http://www.omdbapi.com/?t='+ encodeURIComponent(result.movieName) + '&y=&plot=short&r=json', function (error, response, body){
-    if (!error && response.statusCode == 200) {
-      var bodyObject = JSON.parse(body);
-      var synopsisArray = [bodyObject.Title, bodyObject.Released, bodyObject.Plot];
-      console.log(synopsisArray);
-      console.log("Is this the movie you were looking for?")
-    prompt.start();
-    prompt.get(['response'], function (err, result) {
-      // console.log();
+var bodyObject;
+function searchMovie(){
+  console.log("Type a movie title!")
+  prompt.start();
+  prompt.get(['movieName'], function (err, result) {
+    var request = require('request');
+    request('http://www.omdbapi.com/?t='+ encodeURIComponent(result.movieName) + '&y=&plot=short&r=json', function (error, response, body){
+      if (!error && response.statusCode == 200) {
+        bodyObject = JSON.parse(body);
+        var synopsisArray = [bodyObject.Title, bodyObject.Released, bodyObject.Plot];
+        console.log(synopsisArray);
+        confirmSelection();
+      }
     })
+  })
+}
+
+function confirmSelection(response, callBack){
+  console.log("Is this the movie you were looking for?  Y/N")
+  prompt.start();
+  prompt.get(['response'], function (err, result) {
+    if ( result.response === 'Y'){
+      var urlThing = bodyObject.Poster;
+      console.log("Have a nice day!");
+      open(urlThing);
+    }
+    else if(result.response === 'N'){
+      console.log("Sorry about that. Let's try again!")
+      callBack = searchMovie();
     }
   })
-})
+}
+
+console.log("Welcome to the Movie Info Finder!");
+searchMovie();
+
+
+
+
+
+
+
+
+
+
+
+// 1. Welcome the user to our application
+
+// 2. Prompt the user to select a movie
 
 // 4. More specifically return movie name, release date and plot.
 
