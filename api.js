56f23e16a8ab31e7ca27accd823005c646cb77c4
Susannah Waite <susannahwaite2@gmail.com>
2016-06-17 10:50:03 -0500

    step one complete.

--- api.js
+++ api.js
@@ -1,5 +1,13 @@
 // 1. Welcome the user to our application
-
+console.log("Welcome to the Movie Info Finder!");
 // 2. Prompt the user to select a movie
 
 // 3. Make a GET request to the OMDb API for the movie information
+
+// 4. More specifically return movie name, release date and plot.
+
+// 5. Prompt the user if this is the movie they were looking for
+
+// 6. If yes open movie poster in the browser
+
+// 7. If no, prompt them for new movie.
