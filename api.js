6016db19d6befca1bce8d7d1449f13b91494272f
Susannah Waite <susannahwaite2@gmail.com>
2016-06-17 11:02:54 -0500

    added step 2 prompt

--- api.js
+++ api.js
@@ -1,7 +1,12 @@
+var prompt = require('prompt');
+
 // 1. Welcome the user to our application
 console.log("Welcome to the Movie Info Finder!");
 // 2. Prompt the user to select a movie
-
+prompt.start();
+prompt.get(['movieName'], function (err, result) {
+  console.log('You picked ' + result.movieName + '!');
+})
 // 3. Make a GET request to the OMDb API for the movie information
 
 // 4. More specifically return movie name, release date and plot.
